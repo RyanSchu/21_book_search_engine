8dbce7eb6e9639cbd9e212ba054a1f4ba0445184
Ryan Schubert <31641912+RyanSchu@users.noreply.github.com>
2022-11-20 21:24:54 -0600

    one more try

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -21,8 +21,7 @@ const SavedBooks = () => {
     return(<h2>Loading...</h2>)
   }
   
-  console.log(likedBooks)
-  const userData = data?.me
+  const userData = likedBooks
 
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
@@ -42,7 +41,28 @@ const SavedBooks = () => {
 
   // if data isn't here yet, say so
 
+  const bookCard = (book) => {
+    return(
+      <Card key={book.bookId} border='dark'>
+        {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+        <Card.Body>
+          <Card.Title>{book.title}</Card.Title>
+          <p className='small'>Authors: {book.authors}</p>
+          <Card.Text>{book.description}</Card.Text>
+          <Button className='btn-block btn-danger' onClick={() => {handleDeleteBook(book.bookId)}}>
+            Delete this Book!
+          </Button>
+        </Card.Body>
+      </Card>
+    )
+  }
 
+  const list = (likedBooks) => {
+    return(
+      likedBooks.map(book => bookCard(book))
+      )
+  }
+  
   return (
     <>
       <Jumbotron fluid className='text-light bg-dark'>
@@ -57,27 +77,12 @@ const SavedBooks = () => {
          (
           <Container>
             <h2>
-            {userData.savedBooks.length
-              ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
+            {userData.length
+              ? `Viewing ${userData.length} saved ${userData.length === 1 ? 'book' : 'books'}:`
               : 'You have no saved books!'}
             </h2>
                   <CardColumns>
-                  {likedBooks.map((book) => {
-
-                    return (
-                      <Card key={book.bookId} border='dark'>
-                        {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
-                        <Card.Body>
-                          <Card.Title>{book.title}</Card.Title>
-                          <p className='small'>Authors: {book.authors}</p>
-                          <Card.Text>{book.description}</Card.Text>
-                          <Button className='btn-block btn-danger' onClick={() => {handleDeleteBook(book.bookId)}}>
-                            Delete this Book!
-                          </Button>
-                        </Card.Body>
-                      </Card>
-                    );
-                  })}
+                      {list(userData)}      
                 </CardColumns>
               </Container>
          )}
